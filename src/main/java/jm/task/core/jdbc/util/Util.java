a4026f065f7329377d304ee92e7aec9a46d68d1c
Alex Sadikov <sav1982@inbox.ru>
2022-11-27 22:51:22 +0300

    added JDBC solution and cleaned code

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,17 +6,11 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соединения с БД
-    private static Connection connection;
     private static final String LOCAL_URL = "jdbc:postgresql://localhost:5432/users";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "123";
 
     public static Connection getConnection() throws SQLException {
-//        if (connection == null) {
-//            connection = DriverManager.getConnection(LOCAL_URL, USERNAME, PASSWORD);
-//        }
-//
-//        return connection;
         return DriverManager.getConnection(LOCAL_URL, USERNAME, PASSWORD);
     }
 }
