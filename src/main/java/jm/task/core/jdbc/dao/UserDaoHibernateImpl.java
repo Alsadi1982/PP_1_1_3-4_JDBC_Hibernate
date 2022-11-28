1e0aa520a5bf2a76b1a741aef225c97c4316e36f
Alex Sadikov <sav1982@inbox.ru>
2022-11-29 00:24:39 +0300

    added Hibernate solution

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 
 import java.util.List;
 
@@ -13,30 +18,108 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        String hql = "create table if not exists users (" +
+                "id bigserial," +
+                "name varchar(100)," +
+                "last_name varchar(100)," +
+                "age smallint);";
+        Transaction transaction = null;
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query query = session.createSQLQuery(hql);
+            query.executeUpdate();
+            transaction.commit();
+
+
+        } catch (Exception ex) {
+            if (transaction == null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to .createUsersTable", ex);
+        }
+
     }
 
     @Override
     public void dropUsersTable() {
 
+        String hql = "drop table if exists users;";
+        Transaction transaction = null;
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createSQLQuery(hql);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction == null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to .dropUsersTable", ex);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
+        Transaction transaction = null;
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction == null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to .saveUser name = " + name, ex);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user == null) {
+                throw new RuntimeException("User not found!");
+            }
+            session.delete(user);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction == null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to .removeUserById id = " + id, ex);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users;
+        Transaction transaction = null;
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery("from User", User.class);
+            users = query.getResultList();
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction == null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to .getAllUsers", ex);
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        dropUsersTable();
+        createUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соединения с БД
+    private static SessionFactory sessionFactory;
     private static final String LOCAL_URL = "jdbc:postgresql://localhost:5432/users";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "123";
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            try {
+                Configuration config = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "org.postgresql.Driver");
+                properties.put(Environment.URL, LOCAL_URL);
+                properties.put(Environment.USER, USERNAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL82Dialect");
+//                properties.put(Environment.HBM2DDL_AUTO, "");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                config.setProperties(properties);
+                config.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(config.getProperties()).build();
+
+                sessionFactory = config.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception ex) {
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+
+        return sessionFactory;
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(LOCAL_URL, USERNAME, PASSWORD);
